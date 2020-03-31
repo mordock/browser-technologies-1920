2f87572a762215120dd142a9d649b79534eed014
DESKTOP-2CCSS00\Max <maxweijers@hotmail.nl>
2020-03-31 11:38:26 +0200

    stuff

--- public/js/colorChange.js
+++ public/js/colorChange.js
@@ -7,6 +7,7 @@ let q3v = document.getElementById('q3Vrouw');
 let q3Label = document.getElementById('q3Label');
 let q4 = document.getElementById('q4').value;
 let q4Label = document.getElementById('q4Label');
+let q5 = document.getElementById('q5');
 let q6 = document.getElementById('q6');
 let q6Label = document.getElementById('q6Label');
 let q7 = document.getElementById('q7');
@@ -23,16 +24,29 @@ let q9Label = document.getElementById('q9Label');
 let numberGreen = 0;
 const totalNumberGreen = 9;
 
+let progressBar = document.getElementById('progress');
+const dateButton = document.getElementById('dateButton');
+
+let q1Green = true, q2Green = true, q3Green = true, q4Green = true, q5Green = true, q6Green = true, q7Green = true, q8Green = true, q9Green = true;
+
 window.onload = function(){
     function test(){
-        console.log(numberGreen);
+
+        console.log("4 " + q4Green);
+        console.log("5 " + q5Green);
+
+        progressBarChange();
         //voornaam
         if(q1.value === ''){
             q1Label.style.backgroundColor = 'ivory';
         }else{
             if(q1.value.length >= 2){
                 q1Label.style.backgroundColor = 'green';
-                numberGreen++;
+
+                if(q1Green == true){
+                    numberGreen++;
+                    q1Green = false;
+                }
             }
         }
 
@@ -42,32 +56,48 @@ window.onload = function(){
         }else{
             if(q2.value.length >= 2){
                 q2Label.style.backgroundColor = 'green';
-                numberGreen++;
+
+                if(q2Green == true){
+                    numberGreen++;
+                    q2Green = false;
+                }
             }
         }
 
         if(q3m.checked){
             q3Label.style.backgroundColor = 'green';
+
+            if(q3Green == true){
+                numberGreen++;
+                q3Green = false;
+            }
         }else if(q3v.checked){
             q3Label.style.backgroundColor = 'green';
+
+            if(q3Green == true){
+                numberGreen++;
+                q3Green = false;
+            }
         }
 
         //date
-        if(!q4){
-            q4Label.style.backgroundColor = 'ivory';
-        }else{
-            q4Label.style.backgroundColor = 'green';
-            numberGreen++;
-        }
+        dateButton.addEventListener('click', checkDate);
 
         //color
+        q5.addEventListener('input', watchColorPicker, false);
 
         //phone
         if(q6.value === ''){
             q6Label.style.backgroundColor = 'ivory';
         }else{
-            q6Label.style.backgroundColor = 'green';
-            numberGreen++;
+            if(q6.value.length === 10){
+                q6Label.style.backgroundColor = 'green';
+
+                if(q6Green == true){
+                    numberGreen++;
+                    q6Green = false;
+                }
+            }
         }
 
         //number
@@ -75,7 +105,11 @@ window.onload = function(){
             q7Label.style.backgroundColor = 'ivory';
         }else{
             q7Label.style.backgroundColor = 'green';
-            numberGreen++;
+
+            if(q7Green == true){
+                numberGreen++;
+                q7Green = false;
+            }
         }
 
         //area
@@ -83,22 +117,68 @@ window.onload = function(){
             q8Label.style.backgroundColor = 'ivory';
         }else{
             q8Label.style.backgroundColor = 'green';
-            numberGreen++;
+
+            if(q8Green == true){
+                numberGreen++;
+                q8Green = false;
+            }
         }
 
         //checkbox
         if(q91.checked){
             q9Label.style.backgroundColor = 'green';
+            if(q9Green == true){
+                numberGreen++;
+                q9Green = false;
+            }
         }else if(q92.checked){
             q9Label.style.backgroundColor = 'green';
+            if(q9Green == true){
+                numberGreen++;
+                q9Green = false;
+            }
         }else if(q93.checked){
             q9Label.style.backgroundColor = 'green';
+            if(q9Green == true){
+                numberGreen++;
+                q9Green = false;
+            }
         }else if(q94.checked){
             q9Label.style.backgroundColor = 'green';
+            if(q9Green == true){
+                numberGreen++;
+                q9Green = false;
+            }
         }else if(q95.checked){
             q9Label.style.backgroundColor = 'green';
+            if(q9Green == true){
+                numberGreen++;
+                q9Green = false;
+            }
         }
 
     }
-    this.setInterval(test, 200);
+    this.setInterval(test, 500);
+}
+
+function watchColorPicker(){
+    q5.style.backgroundColor = 'green';
+
+    if(q5Green == true){
+        numberGreen++;
+        q5Green = false;
+    }
+}
+
+function checkDate(){
+    q4Label.style.backgroundColor = 'green';
+
+    if(q4Green == true){
+        numberGreen++;
+        q4Green = false;
+    }
+}
+
+function progressBarChange(){
+    progressBar.value = numberGreen;
 }
