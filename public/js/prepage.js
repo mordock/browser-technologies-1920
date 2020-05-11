ee85a0364cf7b0d02167ea8bac835de09504763b
DESKTOP-2CCSS00\Max <maxweijers@hotmail.nl>
2020-05-11 09:30:17 +0200

    Better brower detect

--- public/js/prepage.js
+++ public/js/prepage.js
@@ -2,7 +2,18 @@ let lastUser = '';
 
 let lastPersonP = document.getElementById('lastPersonP');
 
-if(window.localStorage){
+function testStorage(){
+    var test = 'test';
+    try{
+        localStorage.setItem(test, test);
+        localStorage.removeItem(test);
+        return true;
+    }catch(e){
+        return false
+    }
+}
+
+if(testStorage() === true){
     lastUser = localStorage.getItem('name');
     if(lastUser == null || lastUser === ''){
         lastPersonP.textContent = 'Jij bent de eerste die deze lijst invult';
@@ -10,4 +21,7 @@ if(window.localStorage){
         lastPersonP.textContent = 'Deze persoon heeft voor jou deze lijst ingevuld: ' + lastUser;
         console.log(lastUser);
     } 
+}else{
+    console.log('bob');
+    lastPersonP.textContent = 'Deze browser support geen localstorage of je hebt deze uitgezet, zet deze weer aan als je de volledige ervaring wil.';
 }
